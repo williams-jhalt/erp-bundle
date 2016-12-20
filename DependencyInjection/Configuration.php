b20d01b9da59159bd7a45362160c36ca96ed81c2
johnh <johnh@DESKTOP-B7JMS42.hq.williamstradingco.com>
2016-12-20 11:36:25 -0500

    Initial Import

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -5,27 +5,26 @@ namespace Williams\ErpBundle\DependencyInjection;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
-class Configuration implements ConfigurationInterface
-{
-    public function getConfigTreeBuilder()
-    {
+class Configuration implements ConfigurationInterface {
+
+    public function getConfigTreeBuilder() {
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('williams_erp');
 
         $rootNode->children()
                 ->scalarNode('role')->end()
                 ->arrayNode('erp_server')
-                    ->children()
-                        ->scalarNode('host')->end()
-                        ->scalarNode('username')->end()
-                        ->scalarNode('password')->end()
-                        ->scalarNode('company')->end()
-                        ->scalarNode('appname')->end()
-                    ->end()
+                ->children()
+                ->scalarNode('host')->end()
+                ->scalarNode('username')->end()
+                ->scalarNode('password')->end()
+                ->scalarNode('company')->end()
+                ->scalarNode('appname')->end()
                 ->end()
-            ->end();
+                ->end()
+                ->end();
 
         return $treeBuilder;
     }
-    
+
 }
--- DependencyInjection/WilliamsErpExtension.php
+++ DependencyInjection/WilliamsErpExtension.php
@@ -2,7 +2,6 @@
 
 namespace Williams\ErpBundle\DependencyInjection;
 
-use Symfony\Bundle\FrameworkBundle\DependencyInjection\Configuration;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
@@ -14,27 +13,24 @@ class WilliamsErpExtension extends Extension {
         $configuration = new Configuration();
 
         $config = $this->processConfiguration($configuration, $configs);
-        
+
         $loader = new YamlFileLoader();
 
         if ($config['role'] == 'server') {
-            
+
             $container->setParameter('erp.host', $config['erp_server']['host']);
             $container->setParameter('erp.username', $config['erp_server']['username']);
             $container->setParameter('erp.password', $config['erp_server']['password']);
             $container->setParameter('erp.company', $config['erp_server']['company']);
             $container->setParameter('erp.appname', $config['erp_server']['appname']);
-            
+
             $loader->load('server_services.yml');
-            
         } else {
-            
+
             $container->setParameter('erp.host', $config['erp_server']['host']);
-            
+
             $loader->load('client_services.yml');
-            
         }
-        
     }
 
 }
